a1013c8631e52bad81f814197db8570343353b94
Rai5569 <yaelromero281@gmail.com>
2023-01-06 19:15:27 -0600

    Tareas terminadas del nuevo régimen de pre.course

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -81,11 +81,7 @@ function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
-   var suma=0;
-   for(var i=0; i<resultadosTest.length; i++){
-      suma=suma+resultadosTest[i];
-   }
-   return suma/resultadosTest.length;
+   return agregarNumeros(resultadosTest)/resultadosTest.length;
 }
 
 function numeroMasGrande(arrayOfNums) {
